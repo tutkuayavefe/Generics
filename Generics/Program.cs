7735d754abf31e869a1b93a28baa28ee9b0f24a1
Tutku Ayavefe <tutkuayavefe@gmail.com>
2021-02-01 21:27:46 +0100

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("person1");
+            names.Add("person2");
+
+
+            Console.WriteLine(names);
+
+
+            List<string> list = new List<string>();
+            Console.WriteLine(list.Count);
+
+
+        }
+    }
+}
